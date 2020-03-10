003f89bdb8930bd388946d88602d78464b3b462a
billy-zeng <bzeng95@gmail.com>
2020-03-10 01:08:57 -0700

    verifies that Team component can access players from within team data

--- src/components/TeamsList/Team/Team.js
+++ src/components/TeamsList/Team/Team.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Team = props => {
   return(
-    <p>{props.teamData.name}</p>
-    // <p>Team page</p>
+    <ul>
+      <h4>{props.teamData.name}</h4>
+      <li>{props.teamData.players[0].name}</li>
+      <li>{props.teamData.players[1].name}</li>
+      <li>{props.teamData.players[2].name}</li>
+      <li>{props.teamData.players[3].name}</li>
+      <li>{props.teamData.players[4].name}</li>
+    </ul>
   )
 }
 
