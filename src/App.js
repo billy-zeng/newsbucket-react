2d5827f75bd06d2c0a249936d69de11b9733a677
billy-zeng <bzeng95@gmail.com>
2020-03-09 17:05:35 -0700

    clears base react index and app files, brings in react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { withRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Components
+// import Routes from './config/routes';
+// import Nav from './components/Nav/Nav';
+// import Footer from './components/Footer/Footer'
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends React.Component {
+  state = {
+    currentUser: localStorage.getItem('uId')
+  };
+
+  render(){
+    return (
+      <div className="App">
+  
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withRouter(App);
